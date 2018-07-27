a2505f703734def4f0683ffb205a1f5273e5414d
pzync <shkhrgrv@gmail.com>
2018-07-27 16:31:49 +0530

    second trial

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1>This is awesome.</h1>
-    <p>You need to learn making things on your own</p>
+    <p>You need to learn making things on your own.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
