160faf913b0f5ad9ffee4861d822ece7e10cd559
pzync <shkhrgrv@gmail.com>
2018-07-31 10:37:34 +0530

    6-mid made my first styled react component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Card from '../components/Card';
+
 
 const IndexPage = () => (
   <div>
@@ -17,8 +19,8 @@ const IndexPage = () => (
           <img src = {require('../images/logo-swift.png')} width = "50px" />
         </div>  
         <svg width="100%" height="172" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path fill="white">
-            <animate repeatCount = "indefinite" fill = "freeze" attributeName = "d" dur = "10s" values = "M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z;
+        <path fill="white">
+            <animate repeatCount = "indefinite" fill = "freeze" attributeName = "d" dur = "10s" values ="M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z;
               
             M0 86.3149C316 86.315 444 159.155 884 51.1554C1324 -56.8446 1320.29 34.1214 1538 70.4063C1814 116.407 2156 188.408 2560 86.315V232.317L0 232.316V86.3149Z;
               
@@ -32,6 +34,14 @@ const IndexPage = () => (
         </svg>
 
       </div>
+    </div>
+    <div className = "Cards">
+      <h2>11 courses, more coming</h2>
+      <Card 
+        title = "React for Designers"
+        text = "12 Sections"
+        image = {require('../images/wallpaper.jpg')}
+      />
     </div>
   </div>
 )
