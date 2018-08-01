077de527316a54dc60799abf5c8dbc36ced5f610
pzync <shkhrgrv@gmail.com>
2018-08-01 16:36:13 +0530

    completed 7 states and events

--- src/components/header.js
+++ src/components/header.js
@@ -2,19 +2,44 @@ import React from 'react'
 import Link from 'gatsby-link'
 import './Header.css'
 
-const Header = ({ siteTitle }) => (
-  <div>
-    <div className = "Header">
-      <div className = "HeaderGroup">
-        {/* <Link to="/"><img src="https://i.imgur.com/cPLGUTf.png" width="30" /></Link> */}
-        <Link to="/"><img src={require('../images/site-logo.png')} width = "30" /></Link>
-        <Link to="/courses">Courses</Link>
-        <Link to="/downloads">Downloads</Link>
-        <Link to="/workshops">Workshops</Link>
-        <Link to="/buy"><button>Buy</button></Link>
+class Header extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      hasScrolled : false
+    }
+  }
+
+  componentDidMount(){
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50){
+      this.setState ({ hasScrolled:true })
+    } else {
+      this.setState ({ hasScrolled:false })
+    }     // the else is required otherwise the flag once set on scroll down will not reset. i.e. the state will not go back on scrolling up to initial position.
+  }
+
+  render(){
+    return(
+      <div className = { this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header' }>
+        <div className = "HeaderGroup">
+          {/* <Link to="/"><img src="https://i.imgur.com/cPLGUTf.png" width="30" /></Link> */}
+          <Link to="/"><img src={require('../images/site-logo.png')} width = "30" /></Link>
+          <Link to="/courses">Courses</Link>
+          <Link to="/downloads">Downloads</Link>
+          <Link to="/workshops">Workshops</Link>
+          <Link to="/buy"><button>Buy</button></Link>
+        </div>
       </div>
-    </div>
-  </div>
-)
+    )
+  }
+}
+
 
 export default Header
